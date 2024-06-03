16dfa5c2360a78f55cfa86834118626fc9eae129
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-03 20:44:33 -0300

    Edit Form

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,11 @@ Route::get('/tasks/{id}', function ($id){
     return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
+Route::get('/tasks/{id}/edit', function ($id){
+    return view('edit', ['task' => Task::findOrFail($id)]);
+})->name('tasks.edit');
+
+
 
 Route::post('/tasks', function (Request $request){
 
@@ -48,6 +53,26 @@ Route::post('/tasks', function (Request $request){
         ->with('success', 'Task created successfully');
 
 })->name('tasks.store');
+
+Route::put('/tasks/{id}', function ($id, Request $request){
+
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task =  Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task updated successfully');
+
+})->name('tasks.update');
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
